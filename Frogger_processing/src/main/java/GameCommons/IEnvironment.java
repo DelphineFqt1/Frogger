437c185ab63350e6eb356a1bd0683bbce7a8a2e5
Ottogod <alexandre.kha@yahoo.com>
2021-10-30 10:26:52 +0200

    Un peu plus de javadoc

--- Frogger_processing/src/main/java/GameCommons/IEnvironment.java
+++ Frogger_processing/src/main/java/GameCommons/IEnvironment.java
@@ -7,28 +7,30 @@ import java.util.ArrayList;
 public interface IEnvironment {
 
     /**
-     * créé la liste des voitures en rangée i
-     * /
+     * Créé la liste des voitures en rangée spécifiée
+     * @param range
+     *              La rangée commune des instances de Car de la liste
+     * @return Une liste d'instances de Car rangée range, en nombre aléatoire entre 2 et 3, de même vitesse sélectionnée aléatoirement entre 4 crans et de taille aléatoire entre 2 crans
      */
     ArrayList<Car> car_range(int range);
     /**
      * créé la liste des troncs en rangée i
-     * /
+     *
      */
     ArrayList<Trunk> trunk_range(int range);
     /**
      * créé une liste de liste de voitures [[rangée 1], [rangée 2], ..., [range_end]]
-     * /
+     *
      */
     abstract ArrayList<ArrayList<Car>> allCars(int range_end);
     /**
      * créé une liste de liste de troncs [[rangée begin], [rangée 2], ..., [range_end]]
-     * /
+     *
      */
     abstract ArrayList<ArrayList<Trunk>> allTrunks(int range_begin, int range_end);
     /**
      * déplace chaque voiture d'une liste de liste de voitures selon les ordonnées
-     * /
+     *
      */
     abstract void move_allCars(ArrayList<ArrayList<Car>> allCars, int ydir);
 
