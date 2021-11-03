4772cf0f0f10f3f1b351e005c98b632b7b10a155
Delphine <fouquetdelphine@gmail.com>
2021-11-03 19:33:27 +0100

    Derniers tests rajoutés

--- Frogger_processing/test/MovingElements/FrogTest.java
+++ Frogger_processing/test/MovingElements/FrogTest.java
@@ -7,34 +7,60 @@ import static org.junit.jupiter.api.Assertions.*;
 class FrogTest {
 
     @Test
-    void move() {
+    void intersect() {
+        Obstacle.Car car = new Obstacle.Car(1,1,1,1,1,1,1,1);
+        Obstacle.Trunk trunk = new Obstacle.Trunk(2,2,1,1,1,1,1,1);
+        Frog frog = new Frog(1,1,1);
+        assertTrue(frog.intersect(car));
+        assertFalse(frog.intersect(trunk));
     }
 
     @Test
-    void intersect() {
+    void move() {
+        Frog frog = new Frog(1,1,1);
+        frog.move(1,1);
+        assertEquals(2,frog.getLeft());
+        assertEquals(3,frog.getRight());
+        assertEquals(3,frog.getTop());
+        assertEquals(2,frog.getBottom());
     }
 
     @Test
     void isGAMEOVER() {
+        Frog frog = new Frog(1,1,1);
+        assertFalse(frog.isGAMEOVER());
     }
 
     @Test
     void setGAMEOVER() {
+        Frog frog = new Frog(1,1,1);
+        frog.setGAMEOVER(true);
+        assertTrue(frog.isGAMEOVER());
     }
 
     @Test
     void isCar_intersection() {
+        Frog frog = new Frog(1,1,1);
+        assertFalse(frog.isCar_intersection());
     }
 
     @Test
     void setCar_intersection() {
+        Frog frog = new Frog(1,1,1);
+        frog.setCar_intersection(true);
+        assertTrue(frog.isCar_intersection());
     }
 
     @Test
     void isTrunk_intersection() {
+        Frog frog = new Frog(1,1,1);
+        assertTrue(frog.isTrunk_intersection());
     }
 
     @Test
     void setTrunk_intersection() {
+        Frog frog = new Frog(1,1,1);
+        frog.setTrunk_intersection(false);
+        assertFalse(frog.isTrunk_intersection());
     }
 }
--- Frogger_processing/test/MovingElements/ObstacleTest.java
+++ Frogger_processing/test/MovingElements/ObstacleTest.java
@@ -8,13 +8,23 @@ class ObstacleTest {
 
     @Test
     void getAbs_limit() {
+        Obstacle.Car obstacle = new Obstacle.Car(1,1,1,1,1,1,1,1);
+        assertEquals(1, obstacle.getAbs_limit());
     }
 
     @Test
     void getOrd_lim() {
+        Obstacle.Car obstacle = new Obstacle.Car(1,1,1,1,1,1,1,1);
+        assertEquals(1, obstacle.getOrd_lim());
     }
 
     @Test
     void move() {
+        Obstacle.Car car = new Obstacle.Car(1,1,1,1,1,1,1,1);
+        car.move(1,1);
+        assertEquals(-1,car.getLeft());
+        assertEquals(0,car.getRight());
+        assertEquals(1,car.getTop());
+        assertEquals(0,car.getBottom());
     }
 }
